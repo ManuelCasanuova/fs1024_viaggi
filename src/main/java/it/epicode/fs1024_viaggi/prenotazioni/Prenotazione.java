ee35111740ea54042f2161ce46a8bd4e7965a4d4
ManuelCasanuova <manuelcasanuova@gmail.com>
2025-05-02 12:20:55 +0200

    modifica nella classe prenotazione, inserimento attributo ID

--- src/main/java/it/epicode/fs1024_viaggi/prenotazioni/Prenotazione.java
+++ src/main/java/it/epicode/fs1024_viaggi/prenotazioni/Prenotazione.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 
 public class Prenotazione {
 
+    private Long id;
     private Long numeroPrenotazione;
     private Dipendente dipendente;
     private Viaggio viaggio;
