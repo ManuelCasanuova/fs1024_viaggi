f7230db7b2826b1df4df853c73c52b7b2fe4414a
ManuelCasanuova <manuelcasanuova@gmail.com>
2025-05-02 11:07:42 +0200

    Initial commit

---
+++ src/main/java/it/epicode/fs1024_viaggi/Fs1024ViaggiApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.fs1024_viaggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fs1024ViaggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fs1024ViaggiApplication.class, args);
+	}
+
+}
